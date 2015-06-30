4307c9ddaa2bcb53966e8a662e933e2b1cec2d74
haledeng <dhlwust@163.com>
2015-06-30 20:02:46 +0800

    导入更新

--- db/insert.js
+++ db/insert.js
@@ -4,17 +4,18 @@
 */
 var Datastore = require('nedb'), 
 	conf = require('../conf.js'),
+	path = require('path'),
 	db = new Datastore({filename: path.join('../', conf.db_path), autoload: true});
 	parse = require('./parse.js');
 
-var db = model.db,
-	iconList = parse.getAllIcons(),
+var iconList = parse.getAllIcons(),
 	insertObj = [];
 
 iconList.forEach(function(item, index){
 	insertObj.push({name: item});
 });
 
+console.log(insertObj);
 db.insert(insertObj, function (err) {
 	if(err) console.log(err);
 });
